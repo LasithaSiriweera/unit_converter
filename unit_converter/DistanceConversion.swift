26386abe546a6c2a8eb8a0802c21eaaa789fbe05
Lasitha Siriweera <lasithasiriweera@Udithas-Mac.local>
2018-08-18 04:32:00 -0700

    add distance converction class

---
+++ unit_converter/DistanceConversion.swift
@@ -0,0 +1,44 @@
+//
+//  DistanceConversion.swift
+//  unit_converter
+//
+//  Created by Lasitha Siriweera on 8/18/18.
+//  Copyright © 2018 Lasitha Siriweera. All rights reserved.
+//
+
+import Foundation
+
+class DistanceConversion{
+   
+    func metreToFoot(distance: Float32)-> Float32{
+        return distance / 0.3
+    }
+    
+    func metreToYard(distance: Float32) -> Float32 {
+        return distance / 0.9
+    }
+    
+    func metreToK(distance: Float32) -> Float32 {
+        return distance * 0.001
+    }
+    
+    func  metreToMile(distance: Float32) -> Float32 {
+        return distance / 1600
+    }
+    
+    func footToMetre(distance: Float32) -> Float32 {
+        return distance * 0.3
+    }
+    
+    func yardToMetre(distance: Float32) -> Float32 {
+        return distance * 0.9
+    }
+    
+    func kmToMetre(distance: Float32) -> Float32 {
+        return distance * 1000
+    }
+    
+    func mileToMetre(distance: Float32) -> Float32 {
+        return distance * 1600
+    }
+}
---
+++ unit_converter/distanceCoversion.swift
@@ -0,0 +1,9 @@
+//
+//  distanceCoversion.swift
+//  unit_converter
+//
+//  Created by Lasitha Siriweera on 8/18/18.
+//  Copyright © 2018 Lasitha Siriweera. All rights reserved.
+//
+
+import Foundation
